72620566545e6f3cdd2d4a2d29b1f3740171c53e
alina-olaru <olarualina01@gmail.com>
2021-02-07 20:05:42 +0200

    filter based on more fields;

--- mat-table-custom-sort/src/app/app.component.ts
+++ mat-table-custom-sort/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-
-export interface Customer{
+export interface Customer {
   [key: string]: any;
   firstName: string;
   lastName: string;
@@ -18,60 +17,107 @@ export interface Customer{
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent implements OnInit{
-  customers  = [
+export class AppComponent implements OnInit {
+  customers = [
     {
       firstName: 'Joe',
       lastName: 'Backer',
       address: '',
       hasVoucher: true,
-      total: 546.996
+      total: 546.996,
     },
     {
       firstName: 'joe',
       lastName: 'Backer',
       address: '',
       hasVoucher: true,
-      total: 546.996
+      total: 546.996,
     },
     {
       firstName: 'Amanada',
       lastName: 'Cerkavosky',
       address: '',
       hasVoucher: true,
-      total: 19764.23
+      total: 19764.23,
     },
     {
       firstName: 'Joe',
       lastName: 'McFlafer',
       address: '',
       hasVoucher: true,
-      total: 20.5
+      total: 20.5,
     },
     {
       firstName: 'Jonathan',
       lastName: 'Zamborksy',
       address: '',
       hasVoucher: false,
-      total: 437453
+      total: 437453,
     },
   ];
 
   @ViewChild(MatSort)
   sort: MatSort = new MatSort();
   dataSource: MatTableDataSource<Customer>;
-  displayedColumns = [ 'firstName', 'lastName', 'total', 'hasVoucher'];
-  constructor(){
+  displayedColumns = ['firstName', 'lastName', 'total', 'hasVoucher'];
+  constructor() {
     this.dataSource = new MatTableDataSource<Customer>(this.customers);
   }
-  ngOnInit(){
+  ngOnInit() {
     setTimeout(() => {
-    this.dataSource.sort = this.sort;
-    this.dataSource.sortingDataAccessor = (item, property) => {
-      return (item[property].toLocaleLowerCase());
-};
+      this.dataSource.sort = this.sort;
+      this.dataSource.sortingDataAccessor = (item, property) => {
+        return item[property].toLocaleLowerCase();
+      };
+
+      this.dataSource.sortData = (data: Customer[], sort: MatSort) => {
+        const active = sort.active;
+        const direction = sort.direction;
+        return data.sort((a: Customer, b: Customer) => {
+          // Sorting logic here
+          const valueA = this.dataSource.sortingDataAccessor(a, active);
+          const valueB = this.dataSource.sortingDataAccessor(b, active);
+          let comparatorResult = this.compareFunction(valueA, valueB);
+          // if they are equals
+          if (comparatorResult == 0)
+          // we'll sort them using the field lastName
+          {
+            const valueALastName = this.dataSource.sortingDataAccessor(
+              a,
+              'lastName'
+            );
+            const valueBLastName = this.dataSource.sortingDataAccessor(
+              b,
+              'lastName'
+            );
+            // we get the comparator result
+            comparatorResult = this.compareFunction(
+              valueALastName,
+              valueBLastName
+            );
+          }
+          return comparatorResult * (direction == 'asc' ? 1 : -1);
+        });
+      };
+
     });
+  }
+
+  private compareFunction(a: any, b: any): number {
+    let comparatorResult = 0;
+    if (a != null && b != null) {
+      // Check if one value is greater than the other; if equal, comparatorResult should remain 0.
+      if (a > b) {
+        comparatorResult = 1;
+      } else if (a < b) {
+        comparatorResult = -1;
+      }
+    } else if (a != null) {
+      comparatorResult = 1;
+    } else if (b != null) {
+      comparatorResult = -1;
+    }
+    return comparatorResult;
   }
 
   public sortData(): void {
